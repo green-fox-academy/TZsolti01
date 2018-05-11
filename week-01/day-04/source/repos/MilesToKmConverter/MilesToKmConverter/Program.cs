4a94521330df005d89c3cbdfd14c16455dbcde87
TZsolti01 <tarjanyizsolt90@gmail.com>
2018-05-11 13:53:12 +0200

    updated

---
+++ week-01/day-04/source/repos/MilesToKmConverter/MilesToKmConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MilesToKmConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+
+            Console.WriteLine("Can you give me a distance?");
+            int distanceKm = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(distanceKm * 1.60);
+
+            Console.ReadLine();
+          
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
